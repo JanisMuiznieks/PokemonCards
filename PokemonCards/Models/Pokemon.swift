f3050e4398367b1e2376699c38040271744190bc
JanisMuiznieks <janis.muiznieks8@gmail.com>
2021-02-09 15:52:36 +0200

    Pokemon card finder

---
+++ PokemonCards/Models/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  PokemonCards
+//
+//  Created by janis.muiznieks on 09/02/2021.
+//
+
+import Foundation
+
+
+struct Pokemon: Decodable {
+    let name: String
+    var imageUrl: String?
+    let number: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageUrl
+        case number
+    }
+    
+}
+
+struct Card: Decodable {
+    let cards: [Pokemon]
+    
+}
